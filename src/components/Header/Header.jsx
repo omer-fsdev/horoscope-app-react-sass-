0716b31bbdb9e5e7707d4bdaface2c0babc4f212
omer-fsdev <tbp.omerkoyuncu@gmail.com>
2023-05-19 13:46:16 +0200

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">What is your horoscope?</span>
+    </div>
+  );
+};
+
+export default Header;
